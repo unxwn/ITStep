f0958acb5cac100bc8b4b1ccdc7c0ee2b4b24452
Myroslav Polishchuk <myrospolil@gmail.com>
2024-04-06 20:44:17 +0300

    Added Homework 4

---
+++ Lesson4/Homework4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MorseCodeLib;
+
+namespace Homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // To Morse Code
+            Console.WriteLine("Enter the text to be encrypted in Morse Code:");
+            string inputText1 = Console.ReadLine();
+
+            string morseCode = MorseCode.ConvertToMorseCode(inputText1);
+            Console.WriteLine("Encrypting result:");
+            Console.WriteLine(morseCode);
+
+            // To normal alphabet
+            Console.WriteLine("\nEnter the text to be decrypted from Morse Code:");
+            string inputText2 = Console.ReadLine();
+
+            string text = MorseCode.ConvertFromMorseCode(inputText2);
+            Console.WriteLine("Decrypting result:");
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Lesson4/MorseCodeLib/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeLib
+{
+    public class MorseCode
+    {
+        private static char[] normalAlphabet = {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            ' ', ',', '.'
+        };
+
+        private static string[] morseAlphabet = {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+            "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
+            "/", "--..--", ".-.-.-"
+        };
+
+        public static string ConvertToMorseCode(string text)
+        {
+            text = text.Trim().ToLower();
+            string result = "";
+
+            foreach (char c in text)
+            {
+                int index = Array.IndexOf(normalAlphabet, c);
+
+                if (index != -1)
+                {
+                    result += morseAlphabet[index] + " ";
+                }
+            }
+
+            return result;
+        }
+
+        public static string ConvertFromMorseCode(string text)
+        {
+            string[] morseCode = text.Trim().ToLower().Split(' ');
+
+            string result = "";
+
+            foreach (string str in morseCode)
+            {
+                int index = Array.IndexOf(morseAlphabet, str);
+
+                if (index != -1)
+                {
+                    result += normalAlphabet[index];
+                }
+            }
+
+            return result;
+        }
+    }
+}
