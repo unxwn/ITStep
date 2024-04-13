bd15d087a35901f9fb357511eec20704f99fa9b3
Myroslav Polishchuk <myrospolil@gmail.com>
2024-04-13 19:29:09 +0300

    Done Practice6

---
+++ Lesson6/Practice6/Task1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Task1
+{
+    internal abstract class Human
+    {
+        public string Role { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string WorkPlace { get; set; }
+
+        public abstract int RetirementAge { get; }
+
+        public Human() { }
+
+        public Human(string role, string firstName, string lastName, int age, string workPlace)
+        {
+            Role = role;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            WorkPlace = workPlace;
+        }
+
+        public abstract void DoWork();
+        public abstract void ShowYearsToRetireInfo();
+    }
+
+    internal class Builder : Human
+    {
+        public override int RetirementAge { get; } = 65;
+
+        public string ObjectOfWork { get; set; }
+
+        public Builder() { }
+
+        public Builder(string role, string firstName, string lastName, int age, string workPlace, string objectOfWork)
+            : base(role, firstName, lastName, age, workPlace)
+        {
+            ObjectOfWork = objectOfWork;
+        }
+
+        public override void DoWork()
+        {
+            Console.WriteLine($"{Role} {FirstName} {LastName} is building the {ObjectOfWork}");
+        }
+
+        public override void ShowYearsToRetireInfo()
+        {
+            int yearsToRetire = RetirementAge - Age;
+            if (yearsToRetire > 0)
+                Console.WriteLine($"Retirement age: {RetirementAge}, {yearsToRetire} years left until retirement");
+            else
+                Console.WriteLine($"Retirement age: {RetirementAge}, eligible for retirement");
+        }
+    }
+
+    internal class Sailor : Human
+    {
+        public override int RetirementAge { get; } = 55;
+        public string ShipName { get; set; }
+
+        public Sailor() { }
+
+        public Sailor(string role, string firstName, string lastName, int age, string workPlace, string shipName)
+            : base(role, firstName, lastName, age, workPlace)
+        {
+            ShipName = shipName;
+        }
+
+        public override void DoWork()
+        {
+            Console.WriteLine($"{Role} {FirstName} {LastName} is sailing the ship \"{ShipName}\" for {WorkPlace}");
+        }
+        public override void ShowYearsToRetireInfo()
+        {
+            int yearsToRetire = RetirementAge - Age;
+            if (yearsToRetire > 0)
+                Console.WriteLine($"Retirement age: {RetirementAge}, {yearsToRetire} years left until retirement");
+            else
+                Console.WriteLine($"Retirement age: {RetirementAge}, eligible for retirement");
+        }
+    }
+
+    internal class Pilot : Human
+    {
+        public override int RetirementAge { get; } = 50;
+
+        public string Aircraft { get; set; }
+        public string Rank { get; set; }
+
+        public Pilot() { }
+
+        public Pilot(string role, string rank, string firstName, string lastName, int age, string workPlace, string aircraft)
+            : base(role, firstName, lastName, age, workPlace)
+        {
+            Aircraft = aircraft;
+            Rank = rank;
+        }
+
+        public override void DoWork()
+        {
+            Console.WriteLine($"{Role} {Rank} {FirstName} {LastName} is flying the {Aircraft} for {WorkPlace}");
+        }
+        public override void ShowYearsToRetireInfo()
+        {
+            int yearsToRetire = RetirementAge - Age;
+            if (yearsToRetire > 0)
+                Console.WriteLine($"Retirement age: {RetirementAge}, {yearsToRetire} years left until retirement");
+            else
+                Console.WriteLine($"Retirement age: {RetirementAge}, eligible for retirement");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Builder builder = new Builder("Builder", "Ivan", "Petrovych", 65, "Building Company", "Mall");
+            Pilot pilot = new Pilot("Pilot", "Major", "Mykola", "Kamysh", 30, "Armed Forces of Ukraine", "MiG-29MU2");
+            Sailor sailor = new Sailor("Sailor", "Sponge", "Bob", 40, "Travel Company", "Snail");
+
+            Human[] humans = new Human[3];
+            humans[0] = new Builder("Builder", "Ivan", "Petrovych", 65, "Building Company", "Mall");
+            humans[1] = new Pilot("Pilot", "Major", "Mykola", "Kamysh", 30, "Armed Forces of Ukraine", "MiG-29MU2");
+            humans[2] = new Sailor("Sailor", "Sponge", "Bob", 40, "Travel Company", "Snail");
+
+            humans[0].DoWork();
+            humans[0].ShowYearsToRetireInfo();
+
+            Console.WriteLine();
+
+            humans[1].DoWork();
+            humans[1].ShowYearsToRetireInfo();
+
+            Console.WriteLine();
+
+            humans[2].DoWork();
+            humans[2].ShowYearsToRetireInfo();
+        }
+    }
+}
---
+++ Lesson6/Practice6/Task2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Passport
+    {
+        public virtual string PassportType { get; } = "Passport";
+
+        public string PassportID { get; }
+        public string FirstName { get;  }
+        public string LastName { get; }
+        public string BirthDate { get; }
+        public string ExpiryDate { get; }
+
+        public Passport() { }
+
+        public Passport(string id, string firstName, string lastName, string birthDate, string expiryDate)
+        {
+            PassportID = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            ExpiryDate = expiryDate;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"**{PassportType}**");
+            Console.WriteLine($"Passport ID: {PassportID}");
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Date of Birth: {BirthDate}");
+            Console.WriteLine($"Expiry Date: {ExpiryDate}");
+        }
+    }
+
+    internal class ForeignPassport : Passport
+    {
+        public override string PassportType { get; } = "Foreign Passport";
+        public string[] Visas { get; } = new string[10];
+
+        public ForeignPassport(string foreignPassportID, string firstName, string lastName, string birthDate, string expiryDate)
+            : base(foreignPassportID, firstName, lastName, birthDate, expiryDate) { }
+
+        public void AddVisa(string visa)
+        {
+            for (int i = 0; i < Visas.Length; i++)
+            {
+                if (Visas[i] == null)
+                {
+                    Visas[i] = visa;
+                    return;
+                }
+            }
+            Console.WriteLine("No more space for visas!");
+        }
+
+        public void RemoveVisa(string visa)
+        {
+            for (int i = 0; i < Visas.Length; i++) {
+                if (Visas[i] == visa)
+                {
+                    Visas[i] = null;
+                    Console.WriteLine($"Visa ({visa}) removed successfully.");
+                    return;
+                }
+            }
+
+            Console.WriteLine("There is no such visa in the foreign passport.");
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Visas:");
+            foreach (string visa in Visas)
+            {
+                if (visa != null)
+                    Console.WriteLine(visa);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passport ukrainianPassport = new Passport("123456789", "Іван", "Петров", "01.01.2001", "05.05.2035");
+            ukrainianPassport.DisplayInfo();
+
+            Console.WriteLine();
+
+            ForeignPassport foreignPassport = new ForeignPassport("AB1234567", "Ivan", "Petrov", "01.01.2001", "01.01.2031");
+            foreignPassport.AddVisa("USA");
+            foreignPassport.AddVisa("UK");
+            foreignPassport.AddVisa("Canada");
+            foreignPassport.DisplayInfo();
+
+            Console.WriteLine("\nDeleting visa: ");
+            foreignPassport.RemoveVisa("Canada");
+
+            Console.WriteLine();
+            foreignPassport.DisplayInfo();
+        }
+    }
+}
