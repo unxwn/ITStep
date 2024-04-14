d29b42d1c0ebd56feae3b85402c6e73655548cea
Myroslav Polishchuk <myrospolil@gmail.com>
2024-04-14 18:30:46 +0300

    Done Homework6

---
+++ Lesson6/Homework6/Task1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Task1
+{
+    class Money
+    {
+        public string CurrencyType { get; protected set; } = "Not specified";
+        public int MainPart { get; protected set; }
+        public int FractionalPart { get; protected set; }
+
+        public Money() { }
+
+        public Money(string currencyType, int  mainPart, int fractionalPart)
+        {
+            if (currencyType == null || currencyType == "" || mainPart < 0 || fractionalPart < 0 || mainPart <= 0 && fractionalPart <= 0)
+                throw new ArgumentException("Invalid currency type or value");
+
+            CurrencyType = currencyType;
+            MainPart = mainPart;
+            FractionalPart = fractionalPart;
+        }
+
+        public void SetCurrencyType(string currencyType)
+        {
+            if (currencyType == null || currencyType == "")
+            {
+                throw new ArgumentException("Currency type cannot be null or empty");
+            }
+
+            CurrencyType = currencyType;
+        }
+
+        public void SetMainPart(int mainPart)
+        {
+            if (mainPart < 0)
+            {
+                throw new ArgumentException("Main part cannot be negative");
+            }
+
+            MainPart = mainPart;
+        }
+
+        public void SetFractionalPart(int fractionalPart)
+        {
+            if (fractionalPart < 0)
+            {
+                throw new ArgumentException("Fractional part cannot be negative");
+            }
+
+            FractionalPart = fractionalPart;
+        }
+
+        public void ShowCost()
+        {
+            Console.WriteLine($"{MainPart}.{FractionalPart} {CurrencyType}");
+        }
+    }
+
+    class Product : Money
+    {
+        public string Name { get; }
+
+        public Product(string name, int costMainPart, int costFractPart, string currencyType)
+        {
+            Name = name;
+            CurrencyType = currencyType;
+            MainPart = costMainPart;
+            FractionalPart = costFractPart;
+        }
+
+        public void ReduceMainPart(int value)
+        {
+            if (value < 0)
+            { 
+                Console.WriteLine("Value must be positive");
+                return;
+            }
+
+            MainPart -= value;
+        }
+
+        public void ReduceFractPart(int value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Value must be positive");
+                return;
+            }
+
+            FractionalPart -= value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Money class:");
+            Money money = new Money();
+            money.SetCurrencyType("USD");
+            money.SetMainPart(99);
+            money.SetFractionalPart(99);
+            money.ShowCost();
+
+            Console.WriteLine("\nTesting Product class:");
+            Product product = new Product("Pear", 10, 75, "UAH");
+            product.ShowCost();
+            product.ReduceMainPart(5);
+            product.ReduceFractPart(25);
+            product.ShowCost();
+        }
+    }
+}
---
+++ Lesson6/Homework6/Task2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+class Device
+{
+    protected string Name { get; set; }
+    protected string Sound { get; set; }
+
+    public Device(string name, string sound)
+    {
+        Name = name;
+        Sound = sound;
+    }
+    
+    public virtual void GetSound()
+    {
+        Console.WriteLine($"Sound of the {Name}: {Sound}");
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Device name: {Name}");
+    }
+
+    public virtual void ShowDesc()
+    {
+        Console.WriteLine($"Description of {Name}: ...");
+    }
+}
+
+class Kettle : Device
+{
+    public Kettle(string name, string sound) : base(name, sound) { }
+
+    public override void GetSound()
+    {
+        Console.WriteLine("The kettle is whistling");
+    }
+
+    public override void ShowDesc()
+    {
+        Console.WriteLine("Desc: Very hot kettle");
+    }
+}
+
+class Microwave : Device
+{
+    public Microwave(string name, string sound) : base(name, sound)
+    {
+    }
+
+    public override void GetSound()
+    {
+        Console.WriteLine("The microwave is humming");
+    }
+
+    public override void ShowDesc()
+    {
+        Console.WriteLine("Desc: Just a microwave");
+    }
+}
+
+class Car : Device
+{
+    public Car(string name, string sound) : base(name, sound) { }
+
+    public override void GetSound()
+    {
+        Console.WriteLine("The car makes noise");
+    }
+
+    public override void ShowDesc()
+    {
+        Console.WriteLine("Desc: Exspensive and fast car");
+    }
+}
+
+class Steamboat : Device
+{
+    public Steamboat(string name, string sound) : base(name, sound)
+    {
+    }
+
+    public override void GetSound()
+    {
+        Console.WriteLine("The steamboat is whistling (not like a kettle)");
+    }
+
+    public override void ShowDesc()
+    {
+        Console.WriteLine("Desc: Huge steamer");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kettle kettle = new Kettle("Kettle", "Fizz");
+        Microwave microwave = new Microwave("Microwave", "Hum");
+        Car car = new Car("Car", "Buzz");
+        Steamboat steamboat = new Steamboat("Steamboat", "Whlist");
+
+        kettle.Show();
+        kettle.GetSound();
+        kettle.ShowDesc();
+
+        Console.WriteLine();
+
+        microwave.Show();
+        microwave.GetSound();
+        microwave.ShowDesc();
+
+        Console.WriteLine();
+
+        car.Show();
+        car.GetSound();
+        car.ShowDesc();
+
+        Console.WriteLine();
+
+        steamboat.Show();
+        steamboat.GetSound();
+        steamboat.ShowDesc();
+    }
+}
