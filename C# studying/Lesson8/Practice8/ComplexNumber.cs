c050ba19e4adac399c9cd950b0176f522995dc6b
Myroslav Polishchuk <myrospolil@gmail.com>
2024-04-28 15:00:52 +0300

    Updated Practice8

--- C# studying/Lesson8/Practice8/ComplexNumber.cs
+++ C# studying/Lesson8/Practice8/ComplexNumber.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice8
+﻿namespace Practice8
 {
     internal struct ComplexNumber
     {
