55813c50c42f5841715adfa76285cef586c9743e
Myroslav Polishchuk <myrospolil@gmail.com>
2024-05-13 22:11:54 +0300

    Done Homework10

---
+++ C# studying/Lesson10/Homework10/Car.cs
@@ -0,0 +1,46 @@
+﻿using Homework10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Car(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price}";
+        }
+    }
+
+    internal class Comp : IComparer<Car>
+    {
+        public int Compare(Car x, Car y)
+        {
+            if (x.Price < y.Price)
+            {
+                return -1;
+            }
+            else if (x.Price > y.Price)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
