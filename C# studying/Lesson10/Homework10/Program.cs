d04686ce36a3f3720cbb6285d6e3e33a2525f5cf
Myroslav Polishchuk <myrospolil@gmail.com>
2024-05-13 19:40:37 +0300

    Done Homework10

---
+++ C# studying/Lesson10/Homework10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FootballTeam team = new FootballTeam();
+
+            team.AddPlayer(new Player("Bill", 22));
+            team.AddPlayer(new Player("John", 23));
+            team.AddPlayer(new Player("Mike", 25));
+
+            foreach (Player player in team)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
