8166d8d34d1f9259a00d2924495f25e63f73a3cb
Myroslav Polishchuk <myrospolil@gmail.com>
2024-06-08 16:09:20 +0100

    Done HW14

---
+++ C# studying/Lesson14/HW14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 111, 90, 82, 43, 22, 1111 };
+
+            Array.Sort(numbers, (a, b) => SumOfDigits(a).CompareTo(SumOfDigits(b)));
+            Console.WriteLine("Sorting by ascending sum of digits of num: " + string.Join(", ", numbers));
+
+            Array.Sort(numbers, (a, b) => SumOfDigits(b).CompareTo(SumOfDigits(a)));
+            Console.WriteLine("Sorting by descending sum of digits of num: " + string.Join(", ", numbers));
+
+            int SumOfDigits(int number)
+            {
+                int sum = 0;
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                return sum;
+            }
+        }
+    }
+}
