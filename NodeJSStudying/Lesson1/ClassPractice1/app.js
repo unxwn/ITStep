fb700ed260c60e939839cc4f19bdf4f5e3a6ae3b
Myroslav Polishchuk <myrospolil@gmail.com>
2024-07-05 17:03:51 +0100

    Modified ClassPractice1/app.js

--- NodeJSStudying/Lesson1/ClassPractice1/app.js
+++ NodeJSStudying/Lesson1/ClassPractice1/app.js
@@ -3,15 +3,15 @@ const Car = require('./car');
 const http = require('http');
 const url = require('url');
 
-let userInfo = os.userInfo().username;
-console.log(userInfo);
+// let userInfo = os.userInfo().username;
+// console.log(userInfo);
 
-let car = new Car("Audi", "Q7", "2024");
-car.drive();
-car.showInfo();
+// let car = new Car("Audi", "Q7", "2024");
+// car.drive();
+// car.showInfo();
 
 http.createServer(function (request, response) {
-  response.end(url.parse(request.url, true).query.name);
+  response.end(`${url.parse(request.url, true).url}`);
 }).listen(3000, "127.0.0.1", function () {
   console.log("Server is running");
 });
