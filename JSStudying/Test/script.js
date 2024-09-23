d1b2629025e4c123efff88541cb3d4d371ce55a1
Myroslav Polishchuk <myrospolil@gmail.com>
2024-09-23 13:18:53 +0100

    Added eventLoop example

--- JSStudying/Test/script.js
+++ JSStudying/Test/script.js
@@ -1,45 +1,61 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// //! eventLoop
+// setTimeout(function () {
+//   console.log("hihi1");
+// }, 1000);
 
-  greet() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-  }
+// setTimeout(function () {
+//   console.log("hihi2");
+// }, 1000)
 
-  celebrateBirthday() {
-    this.age += 1;
-    console.log(`Happy Birthday to me! Now I am ${this.age} years old.`);
-  }
-}
+// setTimeout(function () {
+//   console.log("hihi3");
+// }, 1000);
 
+// setTimeout(() => console.log("hihi4"), 1000);
 
-function Car(make, model, year) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
+//!
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  this.getDescription = function () {
-    return `${this.year} ${this.make} ${this.model}`;
-  };
-}
+//   greet() {
+//     console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//   }
 
-
-Array.prototype.last = function () {
-  if (this.length === 0) {
-    return undefined;
-  }
-  return this[this.length - 1];
-};
+//   celebrateBirthday() {
+//     this.age += 1;
+//     console.log(`Happy Birthday to me! Now I am ${this.age} years old.`);
+//   }
+// }
 
 
-const person1 = new Person("Alice", 30);
-person1.greet();
-person1.celebrateBirthday();
+// function Car(make, model, year) {
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
 
-const car1 = new Car("Toyota", "Corolla", 2020);
-console.log(car1.getDescription());
+//   this.getDescription = function () {
+//     return `${this.year} ${this.make} ${this.model}`;
+//   };
+// }
 
-const numbers = [1, 2, 3, 4, 5];
-console.log(numbers.last());
+
+// Array.prototype.last = function () {
+//   if (this.length === 0) {
+//     return undefined;
+//   }
+//   return this[this.length - 1];
+// };
+
+
+// const person1 = new Person("Alice", 30);
+// person1.greet();
+// person1.celebrateBirthday();
+
+// const car1 = new Car("Toyota", "Corolla", 2020);
+// console.log(car1.getDescription());
+
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.last());
