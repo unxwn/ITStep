743138b5a27497ae1aa4a84eab1ec0af4f05c260
Myroslav Polishchuk <myrospolil@gmail.com>
2024-10-02 16:59:53 +0100

    Done Lab3

---
+++ SysProgramming/Lesson3/Lab3/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Lesson3
+{
+    internal class Task1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main thread is started");
+            Console.WriteLine("Task1:");
+            List<object> collection = new List<object>
+            {
+                "Hello World",
+                00000,
+                DateTime.Now
+            };
+
+            ParameterizedThreadStart TS = new ParameterizedThreadStart(PerformToString);
+            Thread T = new Thread(TS);
+            T.Start(collection);
+            T.Join();
+
+            Console.WriteLine("Task2:");
+            Bank myBank = new Bank();
+            myBank.UpdateName("MyAcc");
+            myBank.UpdateMoney(1000000);
+            myBank.UpdatePercent(5);
+
+            myBank.UpdateMoney(1500000);
+            myBank.UpdatePercent(6);
+
+            Console.WriteLine("Main thread is ended");
+        }
+
+        static void PerformToString(object obj)
+        {
+            Console.WriteLine("Performing ToString():");
+            if (obj is IEnumerable<object> collection)
+            {
+                foreach (var item in collection)
+                {
+                    string result = item.ToString();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
---
+++ SysProgramming/Lesson3/Lab3/Task2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+public class Bank
+{
+    private readonly string FILE_PATH = "bank_log.txt";
+
+    public int Money { get; private set; }
+    public string Name { get; private set; }
+    public int Percent { get; private set; }
+
+    public void UpdateMoney(int value)
+    {
+        Money = value;
+        LogChanges();
+    }
+
+    public void UpdateName(string value)
+    {
+        Name = value;
+        LogChanges();
+    }
+
+    public void UpdatePercent(int value)
+    {
+        Percent = value;
+        LogChanges();
+    }
+
+    private void LogChanges()
+    {
+        ThreadStart TS = new ThreadStart(() =>
+        {
+            string logEntry = $"Time: {DateTime.Now}, Name: {Name}, Money: {Money}, Percent: {Percent}";
+
+            try
+            {
+                File.AppendAllText(FILE_PATH, logEntry + "\n");
+                Console.WriteLine("Changes saved to the file");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred when saving the data to file: {ex.Message}");
+            }
+        });
+        Thread logThread = new Thread(TS);
+        logThread.Start();
+        logThread.Join();
+    }
+}
