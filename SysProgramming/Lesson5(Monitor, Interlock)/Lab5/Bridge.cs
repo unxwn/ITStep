db0b8e30568c96064a059f003e09529249ad392c
Myroslav Polishchuk <myrospolil@gmail.com>
2024-10-18 17:35:55 +0100

    Done Lab5

---
+++ SysProgramming/Lesson5(Monitor, Interlock)/Lab5/Bridge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab5
+{
+    internal class Bridge
+    {
+        private bool isCrossing = false;
+
+        public void EnterBridge(int carId)
+        {
+            lock (this)
+            {
+                while (isCrossing)
+                {
+                    Console.WriteLine($"Car {carId} is waiting to cross the bridge.");
+                    Monitor.Wait(this);
+                }
+                isCrossing = true;
+                Console.WriteLine($"Car {carId} is crossing the bridge.");
+            }
+        }
+
+        public void ExitBridge(int carId)
+        {
+            lock (this)
+            {
+                isCrossing = false;
+                Console.WriteLine($"Car {carId} has crossed the bridge.");
+                Monitor.Pulse(this);
+            }
+        }
+    }
+}
---
+++ SysProgramming/Lesson5(Monitor, Interlock)/Lab5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static Random rand = new Random();
+        static Bridge bridge = new Bridge();
+
+        static void CarCrossing(object carId)
+        {
+            bridge.EnterBridge((int)carId);
+            Thread.Sleep(rand.Next(1000, 5000));
+            bridge.ExitBridge((int)carId);
+        }
+
+        public static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                new Thread(CarCrossing).Start(i);
+                Thread.Sleep(rand.Next(1000, 5000));
+            }
+        }
+    }
+}
