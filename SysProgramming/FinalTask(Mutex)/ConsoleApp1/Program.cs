023f8e8905b93dea04cfa62f1b4a5b14463ab210
Myroslav Polishchuk <myrospolil@gmail.com>
2024-11-12 13:26:42 +0000

    Done Final task

---
+++ SysProgramming/FinalTask(Mutex)/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool mutexIsOccupied = false;
+            Mutex mutex = null;
+            try
+            {
+                mutex = Mutex.OpenExisting("MyMutexName");
+                Console.WriteLine("Another instance of app is already running ");
+                
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                mutex = new Mutex(true, "MyMutexName");
+                mutexIsOccupied = true;
+
+                Console.WriteLine("It`s first instance of app");
+                Console.WriteLine("Mutex is captured by current thread");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            finally
+            {
+                if (mutexIsOccupied == true)
+                {
+                    mutex.ReleaseMutex();
+                    mutex.Dispose();
+                }
+            }
+
+        }
+    }
+}
