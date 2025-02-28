67143607dc8b15b1b199c3f45c6271aadad3f968
Myroslav Polishchuk <myrospolil@gmail.com>
2025-02-28 12:26:45 +0000

    Fixed problem with connection to DB

--- DataManagement/Lesson6/DataClassLibrary/Data/GamesLibraryContext.cs
+++ DataManagement/Lesson6/DataClassLibrary/Data/GamesLibraryContext.cs
@@ -5,7 +5,7 @@ namespace DataClassLibrary.Data
 {
     public class GamesLibraryContext : DbContext
     {
-        private readonly string _connStr = "Server=(localdb)\\MSSQLLocalDB;Database=GamesLibraryDb;Trusted_Connection=True;";
+        //private readonly string _connStr = "Server=(localdb)\\MSSQLLocalDB;Database=GamesLibraryDb;Trusted_Connection=True;";
 
         public DbSet<Game> Games { get; set; } = null!;
         public DbSet<Studio> Studios { get; set; }
@@ -17,10 +17,10 @@ namespace DataClassLibrary.Data
             Database.EnsureCreated();
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(_connStr);
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(_connStr);
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
