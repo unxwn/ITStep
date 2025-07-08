64ed94de94bf2fcf03fd1245d6713058342a0d2c
Myroslav Polishchuk <myrospolil@gmail.com>
2025-07-08 13:42:41 +0100

    Done online notes project

--- PythonWebDev/online_notes/chronicle/chronicle/urls.py
+++ PythonWebDev/online_notes/chronicle/chronicle/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from apps.core.views import WelcomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('apps.core.urls')),
+    path('', include('apps.core.urls', namespace='core')),  # Простір імен для core
+    path('users/', include('users.urls', namespace='users')),  # Простір імен для users
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
