a18821f494f35df7828a164fe03760f98299f1ca
Myroslav Polishchuk <myrospolil@gmail.com>
2025-07-27 01:19:27 +0100

    Add Lesson4 Class Work

---
+++ CloudTechnologies/Lesson4/AzureMeeting_4/AzureMeeting_4/Functions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+
+namespace AzureMeeting_4
+{
+    public class Functions
+    {
+        public static async Task ProcessQueueMessage(
+            [QueueTrigger("webjobs-test")]string message,
+            [Blob("big-images/{queueTrigger}", FileAccess.Read)]Stream inputFile,
+            [Blob("grayscale-images/gray-{queueTrigger}", FileAccess.Write)]Stream outputFile,
+            ILogger logger
+            )
+        {
+            using(Image image = Image.Load(inputFile))
+            {
+                image.Mutate(x => x.Grayscale(0.8f));
+                await image.SaveAsync(outputFile, new PngEncoder());
+            }
+            await inputFile.CopyToAsync( outputFile );
+
+            logger.LogWarning($"File processed: {message}, Size:  {inputFile.Length} bytes");
+        }
+    }
+}
---
+++ CloudTechnologies/Lesson4/AzureMeeting_4/AzureMeeting_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+Console.WriteLine("Hello, World!");
+HostBuilder builder = new HostBuilder();
+builder.ConfigureWebJobs(configure => {
+    configure.AddAzureStorageBlobs();
+    configure.AddAzureStorageQueues();
+});
+builder.ConfigureLogging((context, configure) =>
+{
+    configure.AddConsole();
+    string? instrumentationKey = context.Configuration.GetValue<string>("AppInsightInstrumentationKey");
+    if (!string.IsNullOrEmpty(instrumentationKey))
+    {
+        configure.AddApplicationInsightsWebJobs(options => {
+            options.InstrumentationKey = instrumentationKey;
+        });
+    }
+});
+var host  = builder.Build();
+using (host)
+{
+    host.Run();
+}
