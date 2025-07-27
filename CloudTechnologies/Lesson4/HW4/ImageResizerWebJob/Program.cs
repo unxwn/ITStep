efea79363fbac43a3ea219008e63240a420c140c
Myroslav Polishchuk <myrospolil@gmail.com>
2025-07-27 01:19:56 +0100

    Add Program.cs file (WebJobs app) Set up configuration (appsettings, User Secrets) Set up logging Set up services (storage initializer service. DI containers, queues) Solve a problem about different objects with the same class in DI Container

---
+++ CloudTechnologies/Lesson4/HW4/ImageResizerWebJob/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights.Extensibility;
+using Azure.Storage.Queues;
+using Azure.Storage.Blobs;
+using ImageResizerWebJob;
+
+var host = new HostBuilder()
+    .ConfigureAppConfiguration((ctx, cfg) =>
+    {
+        cfg.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddUserSecrets<Program>()
+            .AddEnvironmentVariables();
+    })
+    .ConfigureWebJobs(b =>
+    {
+        b.AddAzureStorageBlobs();
+        b.AddAzureStorageQueues();
+    })
+    .ConfigureLogging((ctx, log) =>
+    {
+        log.AddConsole();
+        string aIConnStr = ctx.Configuration.GetConnectionString("ApplicationInsights")
+            ?? throw new InvalidOperationException("You should provide 'ApplicationInsights' in app configuration!");
+
+        if (!string.IsNullOrWhiteSpace(aIConnStr))
+        {
+            log.AddApplicationInsights(
+                configureTelemetryConfiguration: (TelemetryConfiguration tC) =>
+                {
+                    tC.ConnectionString = aIConnStr;
+                },
+                configureApplicationInsightsLoggerOptions: options =>
+                {
+                    options.TrackExceptionsAsExceptionTelemetry = true;
+                }
+            );
+        }
+    })
+    .ConfigureServices((ctx, services) =>
+    {
+        string storageConn = ctx.Configuration.GetConnectionString("AzureStorageAccount")
+                             ?? throw new InvalidOperationException("You should provide 'AzureStorageAccount' in app configuration!");
+
+        if (string.IsNullOrWhiteSpace(storageConn)) return;
+
+        var blobServiceClient = new BlobServiceClient(storageConn);
+        services.AddSingleton(blobServiceClient);
+
+        // НЕ МОЖНА ТОМУ ЩО ОДНАКОВІ КЛАСИ ІН'ЄКТУЮТЬСЯ В DI CONTAINNER І ПЛУТАЮТЬСЯ!!!!!
+        //var inputContainerName = ctx.Configuration["BlobSettings:InputContainerName"]
+        //                         ?? throw new InvalidOperationException("You should provide 'InputContainerName' in app configuration!");
+        //var outputContainerName = ctx.Configuration["BlobSettings:OutputContainerName"]
+        //                          ?? throw new InvalidOperationException("You should provide 'OutputContainerName' in app configuration!");
+
+        //var inputContainer = blobServiceClient.GetBlobContainerClient(inputContainerName);
+        //var outputContainer = blobServiceClient.GetBlobContainerClient(outputContainerName);
+
+        //services.AddSingleton(inputContainer);
+        //services.AddSingleton(outputContainer);
+
+        services.AddHostedService<StorageInitializerService>();
+
+        var queueName = ctx.Configuration["QueueTrigger:QueueName"]
+                        ?? throw new InvalidOperationException("You should provide 'QueueName' in app configuration!");
+
+        var queueClient = new QueueClient(storageConn, queueName);
+
+        services.AddSingleton(queueClient);
+    })
+    .Build();
+
+await host.RunAsync();
